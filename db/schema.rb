b35bcd36e552a4c1e7b5a3a2d2f6440e469bf3b2
D-0bata <d.0000bata@gmail.com>
2026-06-26 08:15:36 +0900

    Taskモデルを修正
    下記コマンドにより、Taskモデルにcountdownフラグを追加した（マイグレーション済み）。
    - rails g migration AddCountdownToTasks countdown:boolean!

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_25_041022) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_25_231026) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "tasks", force: :cascade do |t|
+    t.boolean "countdown", null: false
     t.datetime "created_at", null: false
     t.boolean "done", null: false
     t.string "task_name", null: false
