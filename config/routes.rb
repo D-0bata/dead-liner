286e9c7fc176224d84851947c817fe3d307c3c74
D-0bata <d.0000bata@gmail.com>
2026-06-25 15:03:39 +0900

    Tasksコントローラーを作成
    Tasksコントローラーの作成に伴い、下記のアクションに対応したRoutesとViewsを作成した。
    - index
    - show
    - new
    - create
    - edit
    - update
    - destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
-  get 'hello_world', to: 'hello_world#index'
   root "users#current_or_guest_user"
+
   devise_for :users
+  resources :tasks
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  get "hello_world", to: "hello_world#index"
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
