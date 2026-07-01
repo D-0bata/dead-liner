bc8676b56a9f344bd6de5535e915e5af4724cd3f
D-0bata <d.0000bata@gmail.com>
2026-07-01 09:20:21 +0900

    Taskモデルを修正
    Taskモデルのtask_time列をtime型からinteger型へ修正した。
    change_columnメソッドによるtask_time列の型変換は不可であったため、一度列を削除し、新しく列を追加することで型変換とした。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_25_231026) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_30_060921) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -20,7 +20,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_25_231026) do
     t.boolean "done", null: false
     t.string "task_name", null: false
     t.integer "task_order", null: false
-    t.time "task_time", null: false
+    t.integer "task_time", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_tasks_on_user_id"
