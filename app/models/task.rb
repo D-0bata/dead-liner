76f15ec3255f7f08d24b887bb71d543cbde033f0
D-0bata <d.0000bata@gmail.com>
2026-08-01 18:53:09 +0900

    Taskモデルを更新
    Taskモデルの下記列にバリデーションを追加した。
    - task_order列
    - task_name列
    - task_time列
    - done_flag列
    - timer_flag列
    - timer_started_at列、timer_stopped_at列
      （ActiveSupport::TimeWithZoneクラスのインスタンスであることを検証するため、カスタムメソッド「time_class?」を追加）
    - elapsed_task_time列

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,40 @@
 class Task < ApplicationRecord
   belongs_to :user
+
+  validate :time_class?
+
+  validates :task_order,
+    presence: true,
+    numericality: { only_integer: true, in: 1..255 }
+
+  validates :task_name,
+    presence: true,
+    length: { in: 1..255 }
+
+  validates :task_time,
+    presence: true,
+    numericality: { only_integer: true, in: 1..(24 * 60 * 60) }
+
+  validates :done_flag,
+    inclusion: [ true, false ]
+
+  validates :timer_flag,
+    inclusion: [ true, false ]
+
+  validates :timer_started_at, :timer_stopped_at,
+    presence: true
+
+  validates :elapsed_task_time,
+    presence: true,
+    numericality: { only_integer: true, in: 0.. }
+
+  private
+    def time_class?
+      if !timer_started_at.is_a?(ActiveSupport::TimeWithZone)
+        errors.add(:timer_started_at, "must be an instance of time class")
+      end
+      if !timer_stopped_at.is_a?(ActiveSupport::TimeWithZone)
+        errors.add(:timer_stopped_at, "must be an instance of time class")
+      end
+    end
 end
